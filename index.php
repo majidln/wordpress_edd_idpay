612e31d03e987e641f257b304de2c368b34a3516
Majid Lotfinia <majidlotfinia12@gmail.com>
2018-09-30 16:12:55 +0330

    Update index.php

--- index.php
+++ index.php
@@ -10,234 +10,234 @@
 
 // registers the gateway
 function idpay_edd_register_gateway($gateways) {
-  if(!isset($_SESSION)) {
-    session_start();
-  }
+    if(!isset($_SESSION)) {
+        session_start();
+    }
 
-  $gateways['idpay_edd_gateway'] = array(
-    'admin_label' => 'آیدی پی',
-    'checkout_label' => 'درگاه پرداخت آیدی پی',
-  );
+    $gateways['idpay_edd_gateway'] = array(
+        'admin_label' => 'آیدی پی',
+        'checkout_label' => 'درگاه پرداخت آیدی پی',
+    );
 
-  return $gateways;
+    return $gateways;
 }
 add_filter('edd_payment_gateways', 'idpay_edd_register_gateway');
 
 // disable payment form in checkout
 function idpay_edd_register_gateway_cc_form() {
-  return;
+    return;
 }
 add_action('edd_idpay_edd_gateway_cc_form', 'idpay_edd_register_gateway_cc_form');
 
 // adds the settings to the Payment Gateways section
 function pw_edd_add_settings($settings) {
 
-  $sample_gateway_settings = array(
-    array(
-      'id' => 'idpay_edd_gateway_settings',
-      'type' => 'header',
-      'name' => 'درگاه پرداخت آیدی پی',
-    ),
-    array(
-      'id' => 'idpay_api_key',
-      'type' => 'text',
-      'name' => 'API Key',
-      'size' => 'regular',
-    ),
-    array(
-      'id' => 'idpay_sandbox',
-      'type' => 'checkbox',
-      'name' => 'آزمایشگاه',
-      'default' => 0,
-    )
-  );
+    $sample_gateway_settings = array(
+        array(
+            'id' => 'idpay_edd_gateway_settings',
+            'type' => 'header',
+            'name' => 'درگاه پرداخت آیدی پی',
+        ),
+        array(
+            'id' => 'idpay_api_key',
+            'type' => 'text',
+            'name' => 'API Key',
+            'size' => 'regular',
+        ),
+        array(
+            'id' => 'idpay_sandbox',
+            'type' => 'checkbox',
+            'name' => 'آزمایشگاه',
+            'default' => 0,
+        )
+    );
 
-  return array_merge($settings, $sample_gateway_settings);
+    return array_merge($settings, $sample_gateway_settings);
 }
 add_filter('edd_settings_gateways', 'pw_edd_add_settings');
 
 //
 function gateway_function_to_process_payment($purchase_data) {
-  global $edd_options;
+    global $edd_options;
 
-  $payment_data = array(
-    'price' => $purchase_data['price'],
-    'date' => $purchase_data['date'],
-    'user_email' => $purchase_data['user_email'],
-    'purchase_key' => $purchase_data['purchase_key'],
-    'currency' => $edd_options['currency'],
-    'downloads' => $purchase_data['downloads'],
-    'user_info' => $purchase_data['user_info'],
-    'cart_details' => $purchase_data['cart_details'],
-    'status' => 'pending',
-  );
+    $payment_data = array(
+        'price' => $purchase_data['price'],
+        'date' => $purchase_data['date'],
+        'user_email' => $purchase_data['user_email'],
+        'purchase_key' => $purchase_data['purchase_key'],
+        'currency' => $edd_options['currency'],
+        'downloads' => $purchase_data['downloads'],
+        'user_info' => $purchase_data['user_info'],
+        'cart_details' => $purchase_data['cart_details'],
+        'status' => 'pending',
+    );
 
-  // record the pending payment
-  $payment = edd_insert_payment($payment_data);
+    // record the pending payment
+    $payment = edd_insert_payment($payment_data);
 
-  if(empty($payment)) {
-    edd_send_back_to_checkout('?payment-mode=' . $purchase_data['post_data']['edd-gateway']);
-  }
+    if(empty($payment)) {
+        edd_send_back_to_checkout('?payment-mode=' . $purchase_data['post_data']['edd-gateway']);
+    }
 
-  $api_key = empty($edd_options['idpay_api_key']) ? '' : $edd_options['idpay_api_key'];
-  $sandbox = empty($edd_options['idpay_sandbox']) ? 'false' : 'true';
+    $api_key = empty($edd_options['idpay_api_key']) ? '' : $edd_options['idpay_api_key'];
+    $sandbox = empty($edd_options['idpay_sandbox']) ? 'false' : 'true';
 
-  $amount = idpay_edd_get_amount(intval($purchase_data['price']), edd_get_currency());
-  $desc = 'سفارش شماره #' . $payment;
-  $callback = add_query_arg('verify_idpay_edd_gateway', '1', get_permalink($edd_options['success_page']));
+    $amount = idpay_edd_get_amount(intval($purchase_data['price']), edd_get_currency());
+    $desc = 'سفارش شماره #' . $payment;
+    $callback = add_query_arg('verify_idpay_edd_gateway', '1', get_permalink($edd_options['success_page']));
 
-  if (empty($amount)) {
-    $message = 'واحد پول انتخاب شده پشتیبانی نمی شود.';
-    edd_insert_payment_note($payment, $message);
-    edd_update_payment_status($payment, 'failed');
-    edd_set_error('idpay_connect_error', $message);
-    edd_send_back_to_checkout();
-    return false;
-  }
+    if (empty($amount)) {
+        $message = 'واحد پول انتخاب شده پشتیبانی نمی شود.';
+        edd_insert_payment_note($payment, $message);
+        edd_update_payment_status($payment, 'failed');
+        edd_set_error('idpay_connect_error', $message);
+        edd_send_back_to_checkout();
+        return false;
+    }
 
-  $data = array(
-    'order_id' => $payment,
-    'amount' => $amount,
-    'phone' => '',
-    'desc' => $desc,
-    'callback' => $callback,
-  );
+    $data = array(
+        'order_id' => $payment,
+        'amount' => $amount,
+        'phone' => '',
+        'desc' => $desc,
+        'callback' => $callback,
+    );
 
-  $ch = curl_init('https://api.idpay.ir/v1/payment');
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'X-API-KEY: ' . $api_key,
-    'X-SANDBOX: ' . $sandbox,
-  ));
+    $ch = curl_init('https://api.idpay.ir/v1/payment');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'X-API-KEY: ' . $api_key,
+        'X-SANDBOX: ' . $sandbox,
+    ));
 
-  $result = curl_exec($ch);
-  $result = json_decode($result);
-  $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  curl_close($ch);
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 
-  if ($http_status != 201 || empty($result) || empty($result->link)) {
-    $message = 'هنگام اتصال به درگاه پرداخت خطا رخ داده است';
-    edd_insert_payment_note($payment, $http_status . ' - ' . $message);
-    edd_update_payment_status($payment, 'failed');
-    edd_set_error('idpay_connect_error', $message);
-    edd_send_back_to_checkout();
-    return false;
-  }
+    if ($http_status != 201 || empty($result) || empty($result->link)) {
+        $message = 'هنگام اتصال به درگاه پرداخت خطا رخ داده است';
+        edd_insert_payment_note($payment, $http_status . ' - ' . $message);
+        edd_update_payment_status($payment, 'failed');
+        edd_set_error('idpay_connect_error', $message);
+        edd_send_back_to_checkout();
+        return false;
+    }
 
-  //save id and link
-  edd_insert_payment_note($payment, 'Id: ' . $result->id);
-  edd_insert_payment_note($payment, 'در حال انتقال به درگاه پرداخت');
-  edd_update_payment_meta($payment, 'idpay_payment_id', $result->id);
-  edd_update_payment_meta($payment, 'idpay_payment_link', $result->link);
+    //save id and link
+    edd_insert_payment_note($payment, 'Id: ' . $result->id);
+    edd_insert_payment_note($payment, 'در حال انتقال به درگاه پرداخت');
+    edd_update_payment_meta($payment, 'idpay_payment_id', $result->id);
+    edd_update_payment_meta($payment, 'idpay_payment_link', $result->link);
 
-  $_SESSION['idpay_payment'] = $payment;
+    $_SESSION['idpay_payment'] = $payment;
 
-  //Redirect to payment form
-  //header('Location:' . $result->link);
-  wp_redirect($result->link);
+    //Redirect to payment form
+    //header('Location:' . $result->link);
+    wp_redirect($result->link);
 }
 add_action('edd_gateway_idpay_edd_gateway', 'gateway_function_to_process_payment');
 
 //
 function verify_function_to_process_payment(){
-  global $edd_options;
+    global $edd_options;
 
-  if(empty($_POST['id']) || empty($_POST['order_id'])) return false;
+    if(empty($_POST['id']) || empty($_POST['order_id'])) return false;
 
-  $payment = edd_get_payment($_SESSION['idpay_payment']);
-  unset($_SESSION['idpay_payment']);
+    $payment = edd_get_payment($_SESSION['idpay_payment']);
+    unset($_SESSION['idpay_payment']);
 
-  if (!$payment) {
-    wp_die('اطلاعات ارسال شده صحیح نمی باشد.');
-  }
+    if (!$payment) {
+        wp_die('اطلاعات ارسال شده صحیح نمی باشد.');
+    }
 
-  if ($payment->status == 'complete') return false;
+    if ($payment->status == 'complete') return false;
 
-  $api_key = empty($edd_options['idpay_api_key']) ? '' : $edd_options['idpay_api_key'];
-  $sandbox = empty($edd_options['idpay_sandbox']) ? 'false' : 'true';
+    $api_key = empty($edd_options['idpay_api_key']) ? '' : $edd_options['idpay_api_key'];
+    $sandbox = empty($edd_options['idpay_sandbox']) ? 'false' : 'true';
 
-  // todo: load order_id and id from database!
-  $data = array(
-    'id' => $_POST['id'],
-    'order_id' => $_POST['order_id'],
-  );
+    // todo: load order_id and id from database!
 
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1/payment/inquiry');
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'X-API-KEY: ' . $api_key,
-    'X-SANDBOX: ' . $sandbox,
-  ));
+    $data = array(
+        'id' => $_POST['id'],
+        'order_id' => $payment->ID,
+    );
 
-  $result = curl_exec($ch);
-  $result = json_decode($result);
-  $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.idpay.ir/v1/payment/inquiry');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'X-API-KEY: ' . $api_key,
+        'X-SANDBOX: ' . $sandbox,
+    ));
 
-  if ($http_status != 200) {
-    $message = 'هنگام استعلام پرداخت خطا رخ داده است';
-    edd_insert_payment_note($payment, $http_status . ' - ' . $message);
-    edd_update_payment_status($payment, 'failed');
-    edd_set_error('idpay_connect_error', $message);
-    edd_send_back_to_checkout();
-    return false;
-  }
+    $result = curl_exec($ch);
+    $result = json_decode($result);
+    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 
-  edd_insert_payment_note($payment->ID, 'کد رهگیری آیدی پی: ' . $result->track_id);
-  edd_insert_payment_note($payment->ID, $result->status . ' - ' . idpay_edd_get_error_message($result->status));
-  edd_update_payment_meta($payment->ID, 'idpay_track_id', $result->track_id);
-  edd_update_payment_meta($payment->ID, 'idpay_status', $result->status);
+    if ($http_status != 200) {
+        $message = 'هنگام استعلام پرداخت خطا رخ داده است';
+        edd_insert_payment_note($payment, $http_status . ' - ' . $message);
+        edd_update_payment_status($payment, 'failed');
+        edd_set_error('idpay_connect_error', $message);
+        edd_send_back_to_checkout();
+        return false;
+    }
 
-  if ($result->status == 100) {
-    edd_empty_cart();
-    edd_update_payment_status($payment->ID, 'publish');
-    edd_send_to_success_page();
-  }
-  else {
-    edd_update_payment_status($payment->ID, 'failed');
-    wp_redirect(get_permalink($edd_options['failure_page']));
-  }
+    edd_insert_payment_note($payment->ID, 'کد رهگیری آیدی پی: ' . $result->track_id);
+    edd_insert_payment_note($payment->ID, $result->status . ' - ' . idpay_edd_get_error_message($result->status));
+    edd_update_payment_meta($payment->ID, 'idpay_track_id', $result->track_id);
+    edd_update_payment_meta($payment->ID, 'idpay_status', $result->status);
+
+    if ($result->status == 100) {
+        edd_empty_cart();
+        edd_update_payment_status($payment->ID, 'publish');
+        edd_send_to_success_page();
+    }
+    else {
+        edd_update_payment_status($payment->ID, 'failed');
+        wp_redirect(get_permalink($edd_options['failure_page']));
+    }
 }
 add_action('edd_verify_idpay_edd_gateway', 'verify_function_to_process_payment');
 
 //
 function idpay_edd_get_amount($amount, $currency) {
-  switch (strtoupper($currency)) {
-    case 'IRR':
-    case 'RIAL':
-      return $amount;
+    switch (strtolower($currency)) {
+        case strtolower('IRR') || strtolower('RIAL'):
+            return $amount;
 
-    case 'IRT':
-      return $amount * 10;
+        case strtolower('IRT') || strtolower('TOMAN') || strtolower('Iran TOMAN') || strtolower('Iranian TOMAN') || strtolower('Iran-TOMAN') || strtolower('Iranian-TOMAN') || strtolower('Iran_TOMAN') || strtolower('Iranian_TOMAN') || strtolower('تومان') ||  strtolower('تومان ایران'):
+            return $amount * 10;
 
-    default:
-      return 0;
-  }
+        default:
+            return 0;
+    }
 }
 
 //
 function idpay_edd_get_error_message($code) {
-  switch ($code) {
-    case 1:
-      return 'پرداخت انجام نشده است';
+    switch ($code) {
+        case 1:
+            return 'پرداخت انجام نشده است';
 
-    case 2:
-      return 'پرداخت ناموفق بوده است';
+        case 2:
+            return 'پرداخت ناموفق بوده است';
 
-    case 3:
-      return 'خطا رخ داده است';
+        case 3:
+            return 'خطا رخ داده است';
 
-    case 100:
-      return 'پرداخت تایید شده است';
+        case 100:
+            return 'پرداخت تایید شده است';
 
-    default:
-      return 'کد تعریف نشده است';
-  }
+        default:
+            return 'کد تعریف نشده است';
+    }
 }
 
 /**
@@ -246,8 +246,8 @@ function idpay_edd_get_error_message($code) {
  * @return void
  */
 function listen() {
-  if (isset($_GET['verify_idpay_edd_gateway']) && $_GET['verify_idpay_edd_gateway']) {
-    do_action('edd_verify_idpay_edd_gateway');
-  }
+    if (isset($_GET['verify_idpay_edd_gateway']) && $_GET['verify_idpay_edd_gateway']) {
+        do_action('edd_verify_idpay_edd_gateway');
+    }
 }
 add_action('init', 'listen');
