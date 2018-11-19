4d76a2db39e15fd6cd115a7ff15e64b976b26994
Iman Kiani <kianiman@gmail.com>
2018-11-19 13:00:00 +0330

    fix plugin docblock

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 /**
  * Plugin Name: IDPay for Easy Digital Downloads (EDD)
  * Author: IDPay
- * Description: درگاه پرداخت امن <a href="https://idpay.ir">آیدی پی</a> برای
- * پرداخت در افزودنه دانلود دیجیتال آسان Version: 1.0 Author URI:
- * https://idpay.ir Author Email: info@idpay.ir
+ * Description: درگاه پرداخت امن <a href="https://idpay.ir">آیدی پی</a> برای پرداخت در افزودنه دانلود دیجیتال آسان
+ * Version: 1.0
+ * Author URI: https://idpay.ir
+ * Author Email: info@idpay.ir
  */
 
 include_once('includes/idpay-edd-gateway.php');
