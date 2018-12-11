733e41589139534b17259358c993442064a7dc4a
Iman Kiani <kianiman@gmail.com>
2018-12-11 16:15:22 +0330

    version 1.2

--- idpay-for-edd.php
+++ idpay-for-edd.php
@@ -3,7 +3,7 @@
  * Plugin Name: IDPay for Easy Digital Downloads (EDD)
  * Author: IDPay
  * Description: <a href="https://idpay.ir">IDPay</a> secure payment gateway for Easy Digital Downloads (EDD)
- * Version: 1.1
+ * Version: 1.2
  * Author URI: https://idpay.ir
  * Author Email: info@idpay.ir
  * Tested up to: 5.0
